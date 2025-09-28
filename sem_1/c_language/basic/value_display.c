00cea8c096f3d84bbc4095a609f8474810c46f30
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-09-28 17:57:16 +0530

    basic c program

---
+++ sem_1/c_language/basic/value_display.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n;
+    printf("enter the value of n :");
+    scanf("%d", &n);
+    printf("the value is : %d ",n);
+    getch();
+}
