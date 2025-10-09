1271ae056d8f8c3bb1533eb73eaa486a712258e8
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-09 05:43:37 +0530

    array average

---
+++ sem_1/c_language/1st_assignment/Q5_array_as_parametre.c
@@ -0,0 +1,33 @@
+/*
+. LEVEL- 1 : Arrays as Parameters
+ ■ Objective: Write a program that uses arrays as function parameters.
+ ■ Instructions:
+ □ Create a function that takes an array of integers and its size as parameters.
+ □ The function should calculate and return the average of the elements in the array.
+ □ In the main function, declare an array, initialize it, and call the average function.
+ ■ Sample Output:
+ Average of the array: 25.
+*/
+
+#include<stdio.h>
+int findAverage(int arr[],int size)
+{
+    int avg;
+    int sum=0;
+    for(int i=0;i<5;i++)
+    {
+        sum=sum+arr[i];
+    }
+    avg=sum/size;
+    return avg;
+}
+
+int main()
+{
+    int arr[]={10,20,30,40,50};
+    int size=(sizeof(arr)/sizeof(arr[0]));
+    float avg=findAverage(arr,size);
+    printf("Average of the array: %.2f\n", avg); 
+
+    return 0;
+}
