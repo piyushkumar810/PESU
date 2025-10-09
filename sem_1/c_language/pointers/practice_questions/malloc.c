f0907009bc6d1b17bbfc69636d09468975de6521
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-09 23:07:57 +0530

    malloc

--- sem_1/c_language/pointers/practice_questions/malloc.c
+++ sem_1/c_language/pointers/practice_questions/malloc.c
@@ -1,17 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
     // using malloc
-    int *arr=(int *)malloc(5 * sizeof(int));
-    if(arr==NULL)
-    {
-        printf("memory allocation falied using malloc \n");
+    int *arr = (int *)malloc(5 * sizeof(int));
+
+    if (arr == NULL) {
+        printf("memory allocation failed using malloc\n");
         return 1;
     }
-    for(int i=0;i<5;arr[i++]=i+1);
-    for(int i=0;i<5;i++)
-    {
-        printf("%d ",arr[i]);
+
+    for (int i = 0; i < 5; arr[i++] = i + 1);
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
     }
 }
