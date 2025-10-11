b654ece37043910ef82c1e895007dcb2393b9c9a
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-11 07:01:03 +0530

    revision

---
+++ sem_1/c_language/pointers/practice_questions/singly_linked_list.c
@@ -0,0 +1,38 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// // defining the node structure
+// typedef struct Node{
+//     int data;
+//     struct  Node* link;
+// }; *Node;
+
+
+
+
+// int main()
+// {
+
+// }
+
+// Node create_node(int data)
+// {
+//     node temp=(node) malloc(sizeof(struct node));
+//     if(temp!=null)
+//     {
+//         temp->link=NULL;
+//     }
+//     else
+//     {
+//         printf("unable to create a node")
+//     }
+// }
+
+// node ins_last(node head, int data)
+// if(new_node !=NULL)
+// {
+//     if(Head == Null)
+//     return new_node;
+//     for(temp=head; temp!=link)
+// }
+
