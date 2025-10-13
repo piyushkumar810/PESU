6c0e6f63d897dcbe4bd47c535e246f04885f6453
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-13 20:00:54 +0530

    counting vowels

--- sem_1/python/functions/practice_set_of_function.py
+++ sem_1/python/functions/practice_set_of_function.py
@@ -1,23 +1,23 @@
 # sum of even numbers in your list
 
-# def sum_of_evens(args):
-#     sum=0
-#     for i in args:
-#         if(args % 2==0):
-#             sum=sum+i
-#     return sum
+def sum_of_evens(args):
+    sum=0
+    for i in args:
+        if(i % 2==0):
+            sum=sum+i
+    return sum
 
-# print(sum_of_evens([2,4,6,7,9,3,8,10,12]))
+print(sum_of_evens([2,4,6,7,9,3,8,10,12]))
 
 
 # count the vowels
 print()
-def count_the_vowels(vowels):
-    text='aeiou'
-    count=0
-    for num in vowels:
-        for num in text:
-            count=count+1
+def count_the_vowels(word):
+    vowels = 'aeiou'
+    count = 0
+    for char in word:
+        if char in vowels:
+            count += 1
     return count
 
 print(count_the_vowels('isstring'))
