871343d1628651565e24bad405cc99e14f13e871
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-14 20:48:12 +0530

    calloc in c

---
+++ sem_1/c_language/pointers/practice_questions/calloc.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    // using calloc
+    int *ptr=(int *)calloc(5,sizeof(int));
+    if(ptr==NULL){
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    int i;
+    // if you comment this value entering part then you will see by default initialized values calloc have is (0 0 0 0 0) 
+    printf("enter the values : ");
+    for(i=0; i<5; i++)
+    {
+        scanf("%d ",(ptr+i));
+    }
+
+    printf("the entered values are : ");
+    for(i=0; i<5; i++)
+    {
+        printf("%d ",*(ptr+i));
+    }
+
+    free(ptr);
+    return 0;
+}
---
+++ sem_1/c_language/pointers/practice_questions/tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+printf("enter the values : ");
+    // for(i=0; i<5; i++)
+    // {
+    //     scanf("%d ",(ptr+i));
+    // }
