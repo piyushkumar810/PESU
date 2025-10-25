3792fae46ca005ffb4dd0e732fa8e76952c45df9
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-25 20:42:37 +0530

    merge two SLL

---
+++ sem_1/c_language/3rd_assignment_SLL/Q5_merge_SLL.c
@@ -0,0 +1,12 @@
+/*
+Write a C Program to do the following 
+
+Merge Two Sorted Lists: Write a program that takes two sorted linked lists and merges them to produce a single sorted linked list.
+■ Hint: Use a new list to store the merged result, comparing the current nodes of both lists and appending the smaller one.
+
+typedef struct mynode {
+      int data; 
+      structure dynode *link;
+} *NODE;
+
+*/
