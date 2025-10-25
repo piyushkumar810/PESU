965de199580f30617978030cd37d883e73fa6fde
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-25 20:39:58 +0530

    polynomials

---
+++ sem_1/c_language/3rd_assignment_SLL/Q3_polynomials.c
@@ -0,0 +1,11 @@
+/*
+Write a C Program to do the following 
+
+Addition of Polynomials: Implement a function that adds two polynomials represented as linked lists, where each node contains a coefficient and a degree.
+□ Hint: Traverse both lists simultaneously, summing the coefficients of terms with the same degree. Add the resulting terms to a new polynomial linked list.
+
+typedef struct mynode {
+      int data; 
+      structure dynode *link;
+} *NODE;
+*/
