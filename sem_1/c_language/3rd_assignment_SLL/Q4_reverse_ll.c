8e4a14c45bc988cf6ae74c19ca78ab3cc830842d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-25 20:41:04 +0530

    reverse single linked list

---
+++ sem_1/c_language/3rd_assignment_SLL/Q4_reverse_ll.c
@@ -0,0 +1,12 @@
+/*
+Write a C Program to do the following 
+
+Reverse Linked List: Implement a function that reverses the linked list.
+■ Hint: You may need to keep track of three pointers as you reassign each node’s next pointer to its previous node.
+
+typedef struct mynode {
+      int data; 
+      structure dynode *link;
+} *NODE;
+
+*/
