4461d9329e300792ad0ec8a6b6e73cfdeeaca37f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-10-27 20:07:05 +0530

    prime no

---
+++ sem_1/python/prime_no.py
@@ -0,0 +1,34 @@
+# find prime no between 1-50
+
+# def prime(num):
+#     i=2
+#     while(i<=num/2):
+#         if(num%i==0):
+#             print("this is no a prime value")
+#             return
+#         else:
+#             print(f"{num} is a prime number")
+#         i+=1
+#         return
+
+# n=int(input("enter a value : "))
+# prime(n)
+
+
+# --------------------------- little better way
+def prime(num):
+    if num <= 1:
+        print(f"{num} is not a prime number")
+        return
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print(f"{num} is not a prime number")
+            return
+    print(f"{num} is a prime number")
+
+
+n = int(input("Enter a value: "))
+prime(n)
+
+
