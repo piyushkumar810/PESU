63842cd38bb0233e3d8670d9e0620db746103a78
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-11-01 07:45:20 +0530

    extract unique no form the list

---
+++ sem_1/python/expermintal_learning_unit1_compherension/remove_duplicate_form_list.py
@@ -0,0 +1,6 @@
+# From a list containing duplicate numbers, build a set of unique numbers using comprehensions
+
+list1=[1,20,50,1,40,20,55,203,230,203,70,1]
+
+set_of_unique_no={num for num in list1}
+print(set_of_unique_no)
