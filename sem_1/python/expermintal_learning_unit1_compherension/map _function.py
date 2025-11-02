c42925a1b53f1850d8446ca2332411a9541f1d8e
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-11-02 08:02:46 +0530

    using map function

---
+++ sem_1/python/expermintal_learning_unit1_compherension/map _function.py
@@ -0,0 +1,9 @@
+# Given a list of words, use map() to calculate the length of each word.
+
+# List of words
+words = ["python", "is", "fun", "to", "learn"]
+
+# Use map() to get lengths
+lengths = list(map(len, words))
+
+print(lengths)
