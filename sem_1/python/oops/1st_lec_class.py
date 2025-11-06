1ab032708fd18442c9e768abf692e436ce98718f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-11-06 11:13:20 +0530

    class and object

--- sem_1/python/oops/1st_lec_class.py
+++ sem_1/python/oops/1st_lec_class.py
@@ -1,3 +1,22 @@
+# 🧩 1. Class
+# A class is a blueprint or template that defines how an object will look and behave.
+
+
+# 🚗 2. Object
+# An object is an instance of a class — a real version created from that blueprint.
+# Each object can have its own data.
+
+# 🎯 3. Attributes
+# Attributes are variables inside a class that hold data related to the object.
+# They can be:
+# Class attributes (shared by all objects)
+# Instance attributes (unique to each object)
+
+
+# ⚙️ 4. Methods
+# Methods are functions inside a class that define the behavior of the objects.
+
+
 class student:
     def __init__(self,name,course):
         self.name=name
