16399432baf9b52f48cbeffc08416494c606776d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-11-12 06:08:38 +0530

    some questions

--- sem_1/python/important_question/function_return.py
+++ sem_1/python/important_question/function_return.py
@@ -12,3 +12,14 @@ a, b, c = get_values()
 print("a =", a)
 print("b =", b)
 print("c =", c)
+
+
+# scope of the function
+def outerFunc():
+    sample_str = 'Outer Function'
+    def innerFunc():
+        sample_str = 'Inner Function'
+        print(sample_str)
+    innerFunc()
+    print(sample_str)
+outerFunc()
