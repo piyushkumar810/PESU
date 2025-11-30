f403b66cad4cfb5bd86612157ef7893cd4c59543
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-11-30 22:45:23 +0530

    pallindrom nu

--- sem_1/python/armstrong1.py
+++ sem_1/python/armstrong1.py
@@ -15,3 +15,53 @@ def check_armstrong(num):
 
 inptu=int(input("enter the numbur you want to check: "))
 check_armstrong(inptu)
+
+
+
+
+
+# -------------------- palindrom
+def check_pallindrom(num):
+    sum=0
+    quotp=num
+    while(quotp!=0):
+        r=quotp%10
+        sum=sum*10+r
+        quotp=quotp//10
+
+    if(num==sum):
+        print(f"yes your number {sum} is an pallindrom  number")
+    else:
+        print(f"no this is not pellindrom number")
+
+inptup=int(input("enter the numbur you want to check: "))
+check_pallindrom(inptup)
+
+
+
+
+
+
+
+# ----------------------------------- note
+'''' 
+✔ 1. / — Normal Division (Gives FLOAT)
+/ always gives a decimal number (float).
+
+Example:
+10 / 3   → 3.3333333
+10 / 2   → 5.0
+
+Even if the result is a whole number, Python makes it float.
+
+
+
+✔ 2. // — Floor Division (Gives INTEGER for positive numbers)
+// removes the decimal part and gives the integer part only.
+
+Example:
+10 // 3   → 3
+10 // 2   → 5
+
+This is what we use when we want to cut off the last digit of a number.
+'''
--- sem_1/python/tempCodeRunnerFile.py
+++ sem_1/python/tempCodeRunnerFile.py
@@ -1,9 +1,15 @@
-def outer():
-    x21 = 10
-    def inner():
-        x21 = 20
-    inner()
-    print(x21)
+def check_pallindrom(num):
+    sum=0
+    quotp=num
+    while(quotp!=0):
+        r=quotp%10
+        sum=sum*10+r
+        quotp=quotp//10
 
-outer()
+    if(num==sum):
+        print(f"yes your number {sum} is an pallindrom  number")
+    else:
+        print(f"no this is not pellindrom number")
 
+inptup=int(input("enter the numbur you want to check: "))
+check_pallindrom(inptup)
