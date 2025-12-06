088182271e6496dc10927902ff9080dee2763643
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-12-06 22:03:42 +0530

    use of super keyword

--- sem_1/python/unit2_el(oop)/Q4).py
+++ sem_1/python/unit2_el(oop)/Q4).py
@@ -1,2 +1,35 @@
 # Create a base class Person with name and age, and a subclass Student with roll number and marks.
 # Display full student information using inheritance.d
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_person(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+
+# Derived class
+class Student(Person):
+    def __init__(self, name, age, roll, marks):
+        # Calling Person class constructor
+        super().__init__(name, age)
+        self.roll = roll
+        self.marks = marks
+
+    def display_student(self):
+        # Display Person details
+        self.display_person()
+        # Display Student details
+        print("Roll Number:", self.roll)
+        print("Marks:", self.marks)
+
+
+# Creating a Student object
+s = Student("Piyush", 19, 28, 92)
+
+# Displaying full student info
+s.display_student()
