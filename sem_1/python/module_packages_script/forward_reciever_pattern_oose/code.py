7e233dc7c1f664568497dc1d319e60ea49875a2b
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-12-21 19:27:34 +0530

    changes

--- sem_1/python/module_packages_script/forward_reciever_pattern_oose/code.py
+++ sem_1/python/module_packages_script/forward_reciever_pattern_oose/code.py
@@ -1,25 +1,21 @@
-class Driver:
-    def work(self, msg):
-        return "Ride confirmed: " + msg
+class Car:              # Receiver
+    def reserve(self):
+        return "Car reserved"
 
 
-class TaxiDispatcher:
-    def _init_(self):
-        self.driver = Driver()
+class BookingDispatcher:  # Forwarder
+    def __init__(self):
+        self.car = Car()
 
-    def send(self, msg):
-        print("Dispatcher got the request")
-        return self.driver.work(msg)
-
-
-class Customer:
-    def ask(self, dispatcher, msg):
-        print("Customer sending request")
-        result = dispatcher.send(msg)
-        print("Customer got reply:", result)
+    def send(self):
+        return self.car.reserve()
 
 
-d = TaxiDispatcher()
+class Customer:          # Initiator
+    def ask(self, dispatcher):
+        print(dispatcher.send())
+
+
+d = BookingDispatcher()
 c = Customer()
-
-c.ask(d, "Driver details")
+c.ask(d)
