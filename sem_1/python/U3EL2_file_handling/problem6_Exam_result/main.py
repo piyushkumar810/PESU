2fe06245f635386d514d0d10c1c3baf1c32c9d1a
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-12-23 21:59:40 +0530

    files

---
+++ sem_1/python/U3EL2_file_handling/problem6_Exam_result/main.py
@@ -0,0 +1,70 @@
+# question
+'''
+Question
+
+Write a Python program to merge exam results using two CSV files.
+
+Two CSV files are provided:
+
+theory_marks.csv containing the columns:
+RollNo, Subject, Marks
+
+lab_marks.csv containing the columns:
+RollNo, Subject, Marks
+
+The program should perform the following tasks:
+    Read both CSV files from disk.
+    Calculate the total marks per student by combining theory and lab marks.
+    Create a new CSV file named final_result.csv.
+    Store the following columns in the output file:
+        RollNo
+        TotalMarks
+        Result
+
+    Assign Result as:
+        Pass if TotalMarks ≥ 40
+        Fail otherwise.
+'''
+
+import csv
+total_marks={}
+
+with open("C://Users//piyush kumar//OneDrive//Desktop//GitHub//PESU//sem_1//python//U3EL2_file_handling//problem6_Exam_result//theory_mark.csv","r",newline="") as theory_file:
+    theory_reader=csv.DictReader(theory_file)
+
+    for row in theory_reader:
+        roll=row["RollNo"]
+        mark=int(row["Marks"])
+        
+        if roll in total_marks:
+            total_marks[roll] = total_marks[roll] + mark
+        else:
+            total_marks[roll] = mark
+
+with open("C://Users//piyush kumar//OneDrive//Desktop//GitHub//PESU//sem_1//python//U3EL2_file_handling//problem6_Exam_result//lab_mark.csv","r",newline="") as lab_file:
+    lab_reader=csv.DictReader(lab_file)
+
+    for row in lab_reader:
+        roll=row["RollNo"]
+        mark=int(row["Marks"])
+        
+        if roll in total_marks:
+            total_marks[roll] = total_marks[roll] + mark
+        else:
+            total_marks[roll] = mark
+
+with open("C://Users//piyush kumar//OneDrive//Desktop//GitHub//PESU//sem_1//python//U3EL2_file_handling//problem6_Exam_result//final_result.csv","w",newline="") as file:
+    fieldnames = ["RollNo", "TotalMarks", "Result"]
+    writer=csv.DictWriter(file,fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for roll in total_marks:
+        total=total_marks[roll]
+        result="Pass" if total>=40 else "Fail"
+
+        writer.writerow({
+            "RollNo":roll,
+            "TotalMarks":total,
+            
+        })
---
+++ sem_1/python/module_packages_script/client_dispatcher_server/main.py
@@ -0,0 +1,45 @@
+# ---------------- SERVER ----------------
+class Car:
+    def reserve(self):
+        return "Car reserved successfully"
+
+
+# ---------------- DISPATCHER ----------------
+class BookingDispatcher:
+    def __init__(self):
+        # service registry (name → server object)
+        self.services = {}
+
+    def register_service(self, name, service):
+        self.services[name] = service
+
+    def get_service(self, name):
+        if name in self.services:
+            return self.services[name]
+        else:
+            raise Exception("Requested service not available")
+
+
+# ---------------- CLIENT ----------------
+class Customer:
+    def book_car(self, dispatcher):
+        try:
+            # ask dispatcher for service
+            car_service = dispatcher.get_service("CarService")
+            # use service
+            result = car_service.reserve()
+            print(result)
+        except Exception as e:
+            print(e)
+
+
+# ---------------- MAIN ----------------
+dispatcher = BookingDispatcher()
+
+# server registers itself
+car = Car()
+dispatcher.register_service("CarService", car)
+
+# client requests service
+customer = Customer()
+customer.book_car(dispatcher)
