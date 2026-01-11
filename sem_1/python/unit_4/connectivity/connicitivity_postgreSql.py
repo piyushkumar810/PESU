e775ffd63fd4036a8e9f786633cfe82040a9e882
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-01-11 14:27:39 +0530

    connectivity

--- sem_1/python/unit_4/connectivity/connicitivity_postgreSql.py
+++ sem_1/python/unit_4/connectivity/connicitivity_postgreSql.py
@@ -11,15 +11,24 @@ try:
     # connect to the school database
     conn=psycopg2.connect(
         dbname="School",
-        user="postgre",
-        password="piyush",
+        user="postgres",
+        password="Piyush810",
         host="localhost"
     )
     
+
     if conn:
         print("connection to the postgresql establiched successfuly")
     else:
         print("connection to the postgresql encountered an error")
+
+    '''
+    if conn:
+    --------or----------
+    if conn is not None:
+    print("Connection established")
+    
+    this is better ans readable'''
     
     
     #step 2: open cursor
@@ -27,21 +36,57 @@ try:
     
     
     # step 3: create table
-    cur.execute(''' create table if not exists studnt(
-                student_id serial primary key,
-                student_name varchar(80) not null,
-                grade varchar(50) not null,
-                section varchar(50) not null)
-                ''') 
+    # cur.execute(''' CREATE TABLE students(
+    #     student_id SERIAL PRIMARY KEY,
+    #     student_name VARCHAR(80) NOT NULL,
+    #     grade VARCHAR(10) NOT NULL,
+    #     section VARCHAR(5) NOT NULL
+    #         );''') 
+
+    # print("Table created successfully")
+
+
+    #--------------------------- just inserting some data
+    # print("triyng to insert data")
+
+    # cur.execute('''insert into students(student_name,grade,section)
+    #             values('piyush kumar', 'A', 'MCA-C');
+    #  ''')
     
+    # print("data inserted successfully")
+
+
+    # ----------------------- deleting one record or one piyush kumar data
+    # cur.execute('''
+    #         delete from students where student_id=2;
+    # ''')
+    # print("successfully deleted one record")
+
+
+    # -----------------------------fetcing data
+    print("trying to fetch data")
+
+    cur.execute('''select * from students;
+    ''')
+    print("data fetched successfully")
+    
+    ''' this way the output will not reflect here '''
+
+    # ------------------------- if you want to see the output here
+    rows = cur.fetchall()
+    '''👉 Retrieves all rows from database'''
+
+    # to display it
+    for i in rows:
+        print(i)
+
+
     # step 4:-save changes
     conn.commit()
     
     # step 5:-close connection and cursor
     cur.close()
     conn.close()
-    
-    print("Table created successfully")
 
 except Exception as e:
     print("Database not connected", e)
