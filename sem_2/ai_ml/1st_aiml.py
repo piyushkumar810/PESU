7bb2cc0c12724462a8539475831c7cd3411854b9
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-03-06 13:12:37 +0530

    todays update

---
+++ sem_2/ai_ml/1st_aiml.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+emp_names = ["Piyush", "Rahul", "Ankit", "Priya", "Sneha"]
+
+print(emp_names)
+df=pd.DataFrame(emp_names)
+
+df.columns=['Employee_Name']
+print(df)
+print()
+
+
+data={'Name':["Piyush", "Rahul", "Ankit", "Priya",],
+      'Age':[23,22,22,21]}
+df_dict=pd.DataFrame(data)
+print(df_dict)
---
+++ sem_2/ai_ml/working_with_csv/reading_emp_details.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df=pd.read_csv(r"C:\Users\piyush kumar\OneDrive\Desktop\GitHub\PESU\sem_2\ai_ml\working_with_csv\emp_details.csv")
+
+
+print(df)
+print(df.shape)
