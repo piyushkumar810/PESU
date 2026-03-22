160448844e5dc5f7a8dd75db88451ff1e10449c1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-03-22 17:44:46 +0530

    mcqs on javaScript

---
+++ sem_2/web_application_framworks/javascript_learning/pesu_mcq/predict_the_output.js
@@ -0,0 +1,263 @@
+// 🧠 SECTION: OUTPUT MCQs
+1.
+console.log(1 + "2" + 3);
+
+// A. 123
+// B. 6
+// C. "123"
+// D. "33"
+
+// 👉 Ans: C
+
+
+2.
+console.log("2" + 1 + 3);
+
+// A. "213"
+// B. "24"
+// C. 6
+// D. "231"
+
+// 👉 Ans: A
+
+
+3.
+console.log(1 + 2 + "3");
+
+// A. "33"
+// B. "123"
+// C. 6
+// D. "6"
+
+// 👉 Ans: A
+
+
+4.
+console.log("5" - 2 + "3");
+
+// A. "33"
+// B. 33
+// C. "53"
+// D. 3
+
+// 👉 Ans: A
+
+
+5.
+console.log(true + true);
+
+// A. 2
+// B. true
+// C. "truetrue"
+// D. NaN
+
+// 👉 Ans: A
+
+
+6.
+console.log([] + []);
+
+// A. []
+// B. ""
+// C. 0
+// D. undefined
+
+// 👉 Ans: B
+
+
+// 7.
+// console.log([] + {});
+
+// A. "[object Object]"
+// B. "{}"
+// C. error
+// D. ""
+
+// 👉 Ans: A
+
+// 8.
+// console.log({} + []);
+
+// A. "[object Object]"
+// B. 0
+// C. "[object Object]"
+// D. error
+
+// 👉 Ans: B (TRAP)
+
+// 9.
+// console.log([] == false);
+
+// A. true
+// B. false
+// C. error
+// D. undefined
+
+// 👉 Ans: A
+
+// 10.
+// console.log("" == 0);
+
+// A. true
+// B. false
+// C. error
+// D. NaN
+
+// 👉 Ans: A
+
+// 11.
+// console.log(null == undefined);
+
+// A. true
+// B. false
+// C. error
+// D. NaN
+
+// 👉 Ans: A
+
+// 12.
+// console.log(null === undefined);
+
+// A. true
+// B. false
+// C. error
+// D. NaN
+
+// 👉 Ans: B
+
+// 13.
+// console.log(typeof NaN);
+
+// A. number
+// B. NaN
+// C. undefined
+// D. object
+
+// 👉 Ans: A
+
+// 14.
+// console.log(typeof []);
+
+// A. array
+// B. object
+// C. list
+// D. undefined
+
+// 👉 Ans: B
+
+// 15.
+// console.log([1,2] == "1,2");
+
+// A. true
+// B. false
+// C. error
+// D. undefined
+
+// 👉 Ans: A
+
+// 16.
+// let a = [10,2,1];
+// a.sort();
+// console.log(a[1]);
+
+// A. 2
+// B. 10
+// C. 1
+// D. undefined
+
+// 👉 Ans: B
+
+// 17.
+// console.log("abc".substring(3,1));
+
+// A. "bc"
+// B. "b"
+// C. "cb"
+// D. ""
+
+// 👉 Ans: A
+
+// 18.
+// console.log("abc".charAt(5));
+
+// A. error
+// B. undefined
+// C. ""
+// D. null
+
+// 👉 Ans: C
+
+// 19.
+// console.log("abc".indexOf("z"));
+
+// A. 0
+// B. -1
+// C. undefined
+// D. error
+
+// 👉 Ans: B
+
+// 20.
+// let x = 5;
+// console.log(x++ + ++x);
+
+// A. 11
+// B. 12
+// C. 13
+// D. 14
+
+// 👉 Ans: B
+
+// 21.
+// let a = {};
+// let b = {};
+// console.log(a == b);
+
+// A. true
+// B. false
+// C. error
+// D. undefined
+
+// 👉 Ans: B
+
+// 22.
+// let arr = [];
+// arr[5] = 10;
+// console.log(arr.length);
+
+// A. 5
+// B. 6
+// C. 10
+// D. undefined
+
+// 👉 Ans: B
+
+// 23.
+// console.log(0 || "Hello");
+
+// A. 0
+// B. "Hello"
+// C. true
+// D. false
+
+// 👉 Ans: B
+
+// 24.
+// console.log("5" * "2");
+
+// A. "10"
+// B. 10
+// C. NaN
+// D. error
+
+// 👉 Ans: B
+
+// 25.
+// console.log("5" + 2 * 3);
+
+// A. "56"
+// B. "511"
+// C. "56"
+// D. 11
+
+// 👉 Ans: A
