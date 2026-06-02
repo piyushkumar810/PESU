3b93d1bc671e2d72a71e82f33eca19c7d0bc095d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-06-02 20:01:11 +0530

    updated

--- company_question/coding_solutions/4_check_sorted.py
+++ company_question/coding_solutions/4_check_sorted.py
@@ -2,13 +2,10 @@
 
 
 def check_sorted(arr):
-    if(len(arr)==0):
-        return []
-    else:
-        for i in range(len(arr)-1):
-            if(arr[i]>arr[i+1]):
-                return False
-        return True
+    for i in range(len(arr)-1):
+        if(arr[i]>arr[i+1]):
+            return False
+    return True
 
 
 arr1=[10,30,20,10,34,54,223,23]
