87d8a91c2f6318e7f5f072a38cad09ba812916f7
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-06-08 19:28:32 +0530

    two sum

---
+++ company_question/coding_solutions/Array/9_two_sum.py
@@ -0,0 +1,53 @@
+'''
+Two Sum
+Problem Statement
+
+Given an array of integers nums and an integer target, return the indices of the two numbers such that they add up to the target.
+
+You may assume that:
+
+Exactly one solution exists.
+You cannot use the same element twice.
+
+example:
+nums = [2,7,11,15]
+target = 9
+
+output=[0,1]
+'''
+def two_sum(num, target):
+    l_n = len(num)
+
+    for i in range(l_n):
+        for j in range(i + 1, l_n):
+            if num[i] + num[j] == target:
+                return [i, j]
+
+    return -1
+
+num = [10,20,4,20,3,4,6,3454,64]
+target = int(input("Enter the value: "))
+
+print(two_sum(num, target))
+
+# got 3/5 bad time complexity
+
+# ------------------------ 5/5 better time complexity 
+def two_sum(nums, target):
+    seen = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in seen:
+            return [seen[complement], i]
+
+        seen[nums[i]] = i
+
+    return -1
+
+
+nums = [10, 20, 4, 20, 3, 4, 6, 3454, 64]
+target = int(input("Enter the target value: "))
+
+print(two_sum(nums, target))
--- company_question/coding_solutions/Array/tempCodeRunnerFile.py
+++ company_question/coding_solutions/Array/tempCodeRunnerFile.py
@@ -1,12 +1,12 @@
-def findMissingNumber(arr):
-    n = len(arr)
 
-    for i in range(n):
-        if arr[i] != i + 1:
-            return i + 1
+def two_sum(num,n):
+    l_n=len(num)
 
-    return n + 1
+    for i in range(l_n):
+        for j in range(i+1,l_n):
+            if(num[i]+num[j]==n):
+                return [i,j]
 
-
-arr=[1,2,4,5]
-print(findMissingNumber(arr))
+num=[10,20,4,20,3,4,6,3454,64]
+targrt=int(input("enter the value : "))
+print(two_sum(num,targrt))
