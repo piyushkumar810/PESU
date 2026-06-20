c91807cbefd77b61b29d71682d5915f80ea10bf2
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-06-20 21:14:19 +0530

    updated

--- company_question/coding_solutions/Array/10th_mojarity_element.py
+++ company_question/coding_solutions/Array/10th_mojarity_element.py
@@ -1,6 +1,6 @@
-'''
-Question: Majority Element
+# Question: Majority Element
 
+'''
 Given an integer array `nums` of size `n`, return the majority element.
 The majority element is the element that appears more than `⌊n/2⌋` times.
 You may assume that the majority element always exists in the array.
@@ -31,8 +31,6 @@ Array size = 7
 Element 2 appears 4 times, which is greater than 3.
 Therefore, 2 is the majority element.
 '''
-
-
 def majority_element(nums):
     freq = {}
 
