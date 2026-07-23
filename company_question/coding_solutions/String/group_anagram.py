0ea3271da2b9c39f34fda1afd66cd456362569a2
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-07-23 23:22:56 +0530

    grouping anagram

---
+++ company_question/coding_solutions/String/group_anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups[key].append(s)
+
+        return list(groups.values())
+    
+obj = Solution()
+result = obj.groupAnagrams(["eat", "tea", "bat"])
+print(result)
