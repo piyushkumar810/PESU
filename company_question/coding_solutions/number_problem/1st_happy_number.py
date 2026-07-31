364e10360572000d0ab9c833cb0736ef97f21c30
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-07-31 22:59:58 +0530

    happy number

---
+++ company_question/coding_solutions/number_problem/1st_happy_number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+
+            total = 0
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+
+            n = total
+
+        return n == 1
+
+happy=Solution()
+n=int(input("enter a number: "))
+print(happy.isHappy(n))
