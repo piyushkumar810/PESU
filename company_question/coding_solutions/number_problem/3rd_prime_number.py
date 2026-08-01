149126120cc95ea3ddddcadcae4650ecf1aecaa7
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-01 23:14:25 +0530

    prime number efficient approach

--- company_question/coding_solutions/number_problem/3rd_prime_number.py
+++ company_question/coding_solutions/number_problem/3rd_prime_number.py
@@ -10,3 +10,22 @@ if count == 2:
     print("Prime Number")
 else:
     print("Not Prime")
+
+
+# efficient approach
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    prime = True
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print("Prime Number")
+    else:
+        print("Not Prime")
