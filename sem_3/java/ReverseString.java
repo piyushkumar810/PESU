4c32cbc11c3d53a69cb4ac2d7d4a62b14a41c615
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-13 22:55:36 +0530

    reversing string in java

--- sem_3/java/ReverseString.java
+++ sem_3/java/ReverseString.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
 public class ReverseString {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        // Take input from user
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
+        // Variable to store reversed string
         String reverse = "";
 
+        // Reverse the string
         for (int i = str.length() - 1; i >= 0; i--) {
             reverse = reverse + str.charAt(i);
         }
 
+        // Display result
         System.out.println("Original String: " + str);
         System.out.println("Reversed String: " + reverse);
 
