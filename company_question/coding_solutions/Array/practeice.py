38b7d29f95cd64db03badfbaee3dd9c9dbf2292a
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-16 00:19:38 +0530

    small correction

--- company_question/coding_solutions/Array/practeice.py
+++ company_question/coding_solutions/Array/practeice.py
@@ -15,15 +15,17 @@ print(largest_element(arr))
 
 # 2nd largets element 
 def second_largest(arr):
-    if(len(arr)<1):
+    if(len(arr)<2):
         return None
 
     largest=second_lar=float("-inf")
     for i in arr:
         if(largest<i):
-            largest=i
             second_lar=largest
-        elif(largest)
+            largest=i
+        elif(largest>i>second_lar):
+            second_lar=i
+    return second_lar
 
 arr=[2,4,34,45,56,23]
-print(largest_element(arr))
+print(second_largest(arr))
--- company_question/coding_solutions/Array/tempCodeRunnerFile.py
+++ company_question/coding_solutions/Array/tempCodeRunnerFile.py
@@ -1,12 +1,15 @@
+def second_largest(arr):
+    if(len(arr)<2):
+        return None
 
-def two_sum(num,n):
-    l_n=len(num)
+    largest=second_lar=float("-inf")
+    for i in arr:
+        if(largest<i):
+            second_lar=largest
+            largest=i
+        elif(largest>i>second_lar):
+            second_lar=i
+    return second_lar
 
-    for i in range(l_n):
-        for j in range(i+1,l_n):
-            if(num[i]+num[j]==n):
-                return [i,j]
-
-num=[10,20,4,20,3,4,6,3454,64]
-targrt=int(input("enter the value : "))
-print(two_sum(num,targrt))
+arr=[2,4,34,45,56,23]
+print(second_largest(arr))
