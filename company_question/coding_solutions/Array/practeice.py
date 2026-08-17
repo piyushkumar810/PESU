55ebe7c8c69435db77b25af6111eb4e69710f6fa
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-17 07:13:53 +0530

    practice code of removing duplicate in sorted arr

--- company_question/coding_solutions/Array/practeice.py
+++ company_question/coding_solutions/Array/practeice.py
@@ -29,3 +29,18 @@ def second_largest(arr):
 
 arr=[2,4,34,45,56,23]
 print(second_largest(arr))
+
+# remove duplicate from sorted array
+def remove_duplicate(arr):
+    if(len(arr)==0):
+        return []
+    result=[arr[0]]
+
+    for i in range(1,len(arr)):
+        if(arr[i]!=arr[i-1]):
+            result.append(arr[i])
+    return result
+
+
+arr=[2,4,4,6,6,7,9,12,12,12,15]
+print(remove_duplicate(arr))
--- company_question/coding_solutions/Array/tempCodeRunnerFile.py
+++ company_question/coding_solutions/Array/tempCodeRunnerFile.py
@@ -1,15 +1,13 @@
-def second_largest(arr):
-    if(len(arr)<2):
-        return None
+def remove_duplicate(arr):
+    if(len(arr)==0):
+        return []
+    result=[arr[0]]
 
-    largest=second_lar=float("-inf")
-    for i in arr:
-        if(largest<i):
-            second_lar=largest
-            largest=i
-        elif(largest>i>second_lar):
-            second_lar=i
-    return second_lar
+    for i in range(1,len(arr)):
+        if(arr[i]!=arr[i-1]):
+            result.append(arr[i])
+    return result
 
-arr=[2,4,34,45,56,23]
-print(second_largest(arr))
+
+arr=[2,4,4,6,6,7,9,12,12,12,15]
+print(remove_duplicate(arr))
