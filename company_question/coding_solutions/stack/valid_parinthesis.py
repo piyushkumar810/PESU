d1a577f25e9935e4dc0901fdfe2003422f111459
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-25 23:43:56 +0530

    valid paranthesis in stack

---
+++ company_question/coding_solutions/stack/valid_parinthesis.py
@@ -0,0 +1,16 @@
+def isValid(s: str) -> bool:
+    stack = []
+    pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for ch in s:
+        if ch in pairs:
+            if not stack or stack.pop() != pairs[ch]:
+                return False
+        else:
+            stack.append(ch)
+
+    return not stack
