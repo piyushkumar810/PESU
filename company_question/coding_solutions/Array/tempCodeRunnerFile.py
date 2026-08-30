fa73129a7a48f3a693c27e84270d787a9a745f8e
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-08-30 21:39:00 +0530

    queue using stack

--- company_question/coding_solutions/Array/tempCodeRunnerFile.py
+++ company_question/coding_solutions/Array/tempCodeRunnerFile.py
@@ -1,10 +1,12 @@
-def move_all_zeros(arr):
-    j=0
-    for i in range(len(arr)):
-        if(arr[i]!=0):
-            arr[j],arr[i]=arr[i],arr[j]
-            j+=1
-    return arr
+def findMissingNumber(arr):
+    n = len(arr)
 
-arr=[2,4,0,6,0,7,9,12,0,0,15,0]
-print(move_all_zeros(arr))
+    for i in range(n):
+        if arr[i] != i + 1:
+            return i + 1
+
+    return n + 1
+
+
+arr=[1,2,4,5]
+print(findMissingNumber(arr))
